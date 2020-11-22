d98e044b1ac880ac46df875e3382b7181295ac80
Cherniakhovsky <yurii.cherniakhovskyi@smashdocs.net>
2020-11-22 16:42:06 +0200

    updated view

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from api.models import Image
 from api.minio import MinioHandler
 
 from aiohttp_cors import CorsViewMixin
+
 IMAGE_WIDTH = 800
 
 minioClient = MinioHandler()
@@ -14,10 +15,7 @@ class ImagesView(web.View, CorsViewMixin):
     async def get(self):
         images = await Image.get_images(self.request.app['mongo'])
 
-        image_width = 800
-
-        result = list()
-        for image_data in images:
-            result.append({'imageUrl': minioClient.get_url(image_data, image_width)})
+        result = [{'imageUrl': minioClient.get_url(im_data, IMAGE_WIDTH)}
+                  for im_data in images]
 
         return web.json_response(result)
