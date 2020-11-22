b9e134e8a496c392f397ab1fd3c03aff7482e905
Cherniakhovsky <yurii.cherniakhovskyi@smashdocs.net>
2020-11-22 12:19:50 +0200

    Added test initial cors, may be useless, should be checked

--- api/app.py
+++ api/app.py
@@ -2,17 +2,63 @@ from aiohttp import web
 
 from api.settings import Settings
 from api.views import ImagesView
+
+import aiohttp_cors
 import api.mongo as mongo
 
 
-def setup_routes(app):
-    app.router.add_view('/images', ImagesView)
+# def setup_routes(app):
+#     app.router.add_view('/images/', ImagesView)
 
+# def on_prepare(request, response):
+#     response.headers['Access-Control-Allow-Origin'] = '*'
+#     response.headers['Content-Type'] = 'application/json'
 
 def make_app():
     app = web.Application()
+    # cors = aiohttp_cors.setup(app)
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+    })
+
+    # resource = cors.add(app.router.add_resource("/images/"))
+    # route = cors.add(
+    #     resource.add_route("GET", handler), {
+    #         "http://client.example.org": aiohttp_cors.ResourceOptions(
+    #             allow_credentials=True,
+    #             expose_headers=("X-Custom-Server-Header",),
+    #             allow_headers=("X-Requested-With", "Content-Type"),
+    #             max_age=3600,
+    #         )
+    #     })
+
+    # app.router.add_view('/images/', ImagesView)
+    
+    # import pydevd
+    # pydevd.settrace('192.168.1.229', port=5555, stdoutToServer=True, stderrToServer=True)
+    #
+    # app.on_response_prepare(on_prepare)
+
     app['settings'] = Settings()
-    setup_routes(app)
+
+    # cors.add(app.router.add_view("/images/", ImagesView), webview=True)
+    # cors.add(app.router.add_route("*", "/resource", CorsView), webview=True)
+
+    # setup_routes(app)
+
+    # app.router.add_view('/images/', ImagesView)
+    
+    # import pydevd
+    # pydevd.settrace('192.168.1.229', port=5555, stdoutToServer=True, stderrToServer=True)
+    cors.add(app.router.add_route("*", "/api/images/", ImagesView), webview=True)
+    
+    # for route in list(app.router.routes()):
+    #     cors.add(route)
+
     mongo.setup(app)
     return app
 
--- api/views.py
+++ api/views.py
@@ -3,13 +3,14 @@ from aiohttp import web
 from api.models import Image
 from api.minio import MinioHandler
 
+from aiohttp_cors import CorsViewMixin
 
 minioClient = MinioHandler()
 
 
-class ImagesView(web.View):
+class ImagesView(web.View, CorsViewMixin):
 
-    async def get(self):
+    async def get(self):    
         images = await Image.get_images(self.request.app['mongo'])
 
         image_width = 800
